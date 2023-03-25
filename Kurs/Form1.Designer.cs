1709955600dafdc32d07161c179319520bf2956a
Phalaphun <zhurin.30@gmail.com>
2023-03-25 18:04:43 +1000

    Добавьте файлы проекта.

---
+++ Kurs/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Kurs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.querryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buildingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.audiencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.propertiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materiallyResponsiblesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.departmentsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deansToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.materialsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.citiesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.streetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.tablesToolStripMenuItem,
+            this.querryToolStripMenuItem,
+            this.formsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // tablesToolStripMenuItem
+            // 
+            this.tablesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buildingsToolStripMenuItem,
+            this.audiencesToolStripMenuItem,
+            this.propertiesToolStripMenuItem,
+            this.materiallyResponsiblesToolStripMenuItem,
+            this.departmentsToolStripMenuItem,
+            this.deansToolStripMenuItem,
+            this.materialsToolStripMenuItem,
+            this.citiesToolStripMenuItem,
+            this.streetsToolStripMenuItem});
+            this.tablesToolStripMenuItem.Name = "tablesToolStripMenuItem";
+            this.tablesToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.tablesToolStripMenuItem.Text = "Tables";
+            // 
+            // querryToolStripMenuItem
+            // 
+            this.querryToolStripMenuItem.Name = "querryToolStripMenuItem";
+            this.querryToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.querryToolStripMenuItem.Text = "Querry";
+            // 
+            // formsToolStripMenuItem
+            // 
+            this.formsToolStripMenuItem.Name = "formsToolStripMenuItem";
+            this.formsToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.formsToolStripMenuItem.Text = "Forms";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 250);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(475, 324);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(687, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Refresh";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // buildingsToolStripMenuItem
+            // 
+            this.buildingsToolStripMenuItem.Name = "buildingsToolStripMenuItem";
+            this.buildingsToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.buildingsToolStripMenuItem.Text = "Buildings";
+            this.buildingsToolStripMenuItem.Click += new System.EventHandler(this.buildingsToolStripMenuItem_Click);
+            // 
+            // audiencesToolStripMenuItem
+            // 
+            this.audiencesToolStripMenuItem.Name = "audiencesToolStripMenuItem";
+            this.audiencesToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.audiencesToolStripMenuItem.Text = "Audiences";
+            this.audiencesToolStripMenuItem.Click += new System.EventHandler(this.audiencesToolStripMenuItem_Click);
+            // 
+            // propertiesToolStripMenuItem
+            // 
+            this.propertiesToolStripMenuItem.Name = "propertiesToolStripMenuItem";
+            this.propertiesToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.propertiesToolStripMenuItem.Text = "Properties";
+            this.propertiesToolStripMenuItem.Click += new System.EventHandler(this.propertiesToolStripMenuItem_Click);
+            // 
+            // materiallyResponsiblesToolStripMenuItem
+            // 
+            this.materiallyResponsiblesToolStripMenuItem.Name = "materiallyResponsiblesToolStripMenuItem";
+            this.materiallyResponsiblesToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.materiallyResponsiblesToolStripMenuItem.Text = "Materially Responsibles";
+            this.materiallyResponsiblesToolStripMenuItem.Click += new System.EventHandler(this.materiallyResponsiblesToolStripMenuItem_Click);
+            // 
+            // departmentsToolStripMenuItem
+            // 
+            this.departmentsToolStripMenuItem.Name = "departmentsToolStripMenuItem";
+            this.departmentsToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.departmentsToolStripMenuItem.Text = "Departments";
+            this.departmentsToolStripMenuItem.Click += new System.EventHandler(this.departmentsToolStripMenuItem_Click);
+            // 
+            // deansToolStripMenuItem
+            // 
+            this.deansToolStripMenuItem.Name = "deansToolStripMenuItem";
+            this.deansToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.deansToolStripMenuItem.Text = "Deans";
+            this.deansToolStripMenuItem.Click += new System.EventHandler(this.deansToolStripMenuItem_Click);
+            // 
+            // materialsToolStripMenuItem
+            // 
+            this.materialsToolStripMenuItem.Name = "materialsToolStripMenuItem";
+            this.materialsToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.materialsToolStripMenuItem.Text = "Materials";
+            this.materialsToolStripMenuItem.Click += new System.EventHandler(this.materialsToolStripMenuItem_Click);
+            // 
+            // citiesToolStripMenuItem
+            // 
+            this.citiesToolStripMenuItem.Name = "citiesToolStripMenuItem";
+            this.citiesToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.citiesToolStripMenuItem.Text = "Cities";
+            this.citiesToolStripMenuItem.Click += new System.EventHandler(this.citiesToolStripMenuItem_Click);
+            // 
+            // streetsToolStripMenuItem
+            // 
+            this.streetsToolStripMenuItem.Name = "streetsToolStripMenuItem";
+            this.streetsToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.streetsToolStripMenuItem.Text = "Streets";
+            this.streetsToolStripMenuItem.Click += new System.EventHandler(this.streetsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem querryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formsToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buildingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem audiencesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem propertiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem materiallyResponsiblesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem departmentsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deansToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem materialsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem citiesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem streetsToolStripMenuItem;
+    }
+}
+
---
+++ Kurs/Form1.cs
@@ -0,0 +1,136 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kurs
+{
+    
+    public partial class Form1 : Form
+    {
+        static string connString = "Server=localhost;Port=5432;User ID=postgres;Password=123;Database=University property;";
+        NpgsqlConnection conn = new NpgsqlConnection(connString);
+
+        NpgsqlDataAdapter buildingAdapter;
+        NpgsqlDataAdapter audAdapter;
+        NpgsqlDataAdapter departmentAdapter;
+        NpgsqlDataAdapter citiesAdapter;
+        NpgsqlDataAdapter deansAdapter;
+        NpgsqlDataAdapter materialAdapter;
+        NpgsqlDataAdapter materialResAdapter;
+        NpgsqlDataAdapter propertyAdapter;
+        NpgsqlDataAdapter streetsAdapter;
+
+        DataSet dataSet;
+
+        string currentTable = String.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                conn.Open();
+
+                buildingAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from buildings",conn));
+                audAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from audiences",conn));
+                departmentAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from department", conn));
+                citiesAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from cities_handbook", conn));
+                deansAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from deans_handbook", conn));
+                materialAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from material_handbook", conn));
+                materialResAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from materially_responsible", conn));
+                propertyAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from property", conn));
+                streetsAdapter = new NpgsqlDataAdapter(new NpgsqlCommand("Select * from streets_handbook", conn));
+
+                dataSet = new DataSet();
+                buildingAdapter.Fill(dataSet, "buildings");
+                audAdapter.Fill(dataSet, "audiences");
+                departmentAdapter.Fill(dataSet, "department");
+                citiesAdapter.Fill(dataSet, "cities_handbook");
+                deansAdapter.Fill(dataSet, "deans_handbook");
+                materialAdapter.Fill(dataSet, "material_handbook");
+                materialResAdapter.Fill(dataSet, "materially_responsible");
+                propertyAdapter.Fill(dataSet, "property");
+                streetsAdapter.Fill(dataSet, "streets_handbook");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void buildingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "buildings";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void audiencesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "audiences";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void propertiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "property";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void materiallyResponsiblesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "materially_responsible";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void departmentsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "department";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void deansToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "deans_handbook";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void materialsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "material_handbook";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void citiesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "cities_handbook";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+
+        private void streetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            currentTable = "streets_handbook";
+            dataGridView1.DataSource = dataSet.Tables[currentTable];
+        }
+    }
+}
